0196e3ab24d8c28af85813c2319e7b6b612e0c98
ajaaibu <ajaaibu@gmail.com>
2019-07-15 00:31:58 +0500

    added placeholder

--- components/SearchBar.vue
+++ components/SearchBar.vue
@@ -1,5 +1,5 @@
 <template>
-      <input type="text" class="w-5/6 mx-auto mb-6 rounded-full h-12 pointer-events-auto shadow-md focus:outline-none pl-12 pr-6">
+      <input type="text" placeholder="Search addresses, shops, etc" class="w-5/6 mx-auto mb-6 rounded-full h-12 pointer-events-auto shadow-md focus:outline-none pl-12 pr-6 text-sm">
 </template>
 
 <script>
