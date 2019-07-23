8c9cd43f32a0e7ba9d966e88b4d337c807723936
Abdulla Waheed <iwadday@gmail.com>
2019-07-24 01:53:06 +0500

    Explore dashboard Item

---
+++ components/DashboardIcon.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex justify-center items-center flex-col">
+    <div class="rounded-full h-16 w-16 flex items-center justify-center shadow-lg" :class="bgColor">
+      <icon :icon="icon" class="fill h-8" :class="iconColor"></icon>
+    </div>
+    <h5 class="mt-2 text-sm font-medium text-gray-500">{{ title }}</h5>
+  </div>
+</template>
+
+<script>
+  import Icon from "./Icon";
+  export default {
+    name: 'DashboardIcon',
+    components: {Icon},
+    props: {
+      iconColor: String,
+      bgColor: String,
+      title: {
+        type: String,
+        required: true
+      },
+      icon: {
+        type: String,
+        required: true
+      }
+    }
+  }
+</script>
