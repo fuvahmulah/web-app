19c92ee3aa5122e0b478c33e8bb51e199b12ed57
Abdulla Waheed <iwadday@gmail.com>
2019-07-24 01:56:04 +0500

    Adds search icon to input

--- components/SearchBar.vue
+++ components/SearchBar.vue
@@ -1,11 +1,17 @@
 <template>
-  <input type="text" v-model="keyword" placeholder="Search addresses, shops, etc" class="w-5/6 mx-auto mb-6 rounded-full h-12 pointer-events-auto shadow-md focus:outline-none pl-12 pr-6 text-sm">
+  <div class="relative w-5/6 mx-auto mb-6">
+    <div class="h-6 w-6 absolute" style="top: 13px; left: 15px;">
+      <icon icon="search" class="text-gray-300"></icon>
+    </div>
+    <input type="text" v-model="keyword" placeholder="Search addresses, shops, etc" class="w-full rounded-full h-12 pointer-events-auto shadow-md focus:outline-none pl-12 pr-6 text-sm">
+  </div>
 </template>
 
 <script>
+  import Icon from "./Icon";
   export default {
     name: 'SearchBar',
-
+    components: {Icon},
     data() {
       return {
         keyword: null,
