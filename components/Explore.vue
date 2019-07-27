333814243a839a6597c21fc6a7b236993d5e3917
Abdulla Waheed <iwadday@gmail.com>
2019-07-27 16:48:17 +0500

    Fixes layout margin and width.

--- components/Explore.vue
+++ components/Explore.vue
@@ -1,19 +1,19 @@
 <template>
 <!--  <div class="w-screen h-screen bg-white flex ">-->
-    <div class="absolute top-0 left-0  w-full bg-white h-full p-8 pt-10">
+    <div class="absolute top-0 left-0  w-full bg-white h-full pt-10">
       <button class="absolute right-0 top-0 mt-2 mr-2 p-2 rounded-full h-10 w-10" @click="$emit('close')">
         <icon icon="close" class="text-gray-500"></icon>
       </button>
+      <div class="relative w-5/6 mx-auto mb-6">
       <h4 class="text-xl font-bold leading-none text-gray-800">Explore</h4>
       <h1 class="font-sans text-4xl font-bold leading-none text-gray-800 tracking-wide">Fuvahmulah</h1>
 
-      <flex-row>
+
+      <flex-row class="flex-wrap">
         <dashboard-icon title="Food" icon="cutlery" bg-color="bg-blue-light shadow-food" icon-color="text-white"></dashboard-icon>
         <dashboard-icon title="Attractions" icon="attraction" bg-color="bg-orange-light shadow-attraction" icon-color="text-white"></dashboard-icon>
         <dashboard-icon title="Shops" icon="shop" bg-color="bg-red-500 shadow-shop" icon-color="text-white"></dashboard-icon>
         <dashboard-icon title="Mosques" icon="mosque" bg-color="bg-green-light shadow-masjid" icon-color="text-white"></dashboard-icon>
-      </flex-row>
-      <flex-row>
         <dashboard-icon title="Stay" icon="accommodation" bg-color="bg-green-mid shadow-stay" icon-color="text-white"></dashboard-icon>
         <dashboard-icon title="Services" icon="telephone" bg-color="bg-pink-light shadow-service" icon-color="text-white"></dashboard-icon>
         <dashboard-icon title="Maps" icon="direction" bg-color="bg-purple-600 shadow-map" icon-color="text-white"></dashboard-icon>
@@ -27,6 +27,7 @@
 
       </div>
 
+    </div>
     </div>
 <!--  </div>-->
 </template>
